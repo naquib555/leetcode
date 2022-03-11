8c315814c1d2b5181352de2e5a375ccb1a940ece
Ahmad Chowdhury <naquib555@gmail.com>
2022-03-11 16:33:06 -0500

    Time: 17 ms (90.38%), Space: 49.4 MB (84.73%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.java
@@ -0,0 +1,87 @@
+class MyHashMap {
+
+    private int totalKeySpace;
+    private List<Bucket> hashTable;
+    
+    public MyHashMap() {
+        this.totalKeySpace = 2069;
+        this.hashTable = new ArrayList<Bucket>();
+        
+        for (int i = 0; i < this.totalKeySpace; i++) {
+            this.hashTable.add(new Bucket());
+        }
+    }
+    
+    public void put(int key, int value) {
+        int hashKey = key % this.totalKeySpace;
+        this.hashTable.get(hashKey).update(key, value);
+    }
+    
+    public int get(int key) {
+        int hashKey = key % this.totalKeySpace;
+        return this.hashTable.get(hashKey).get(key);
+    }
+    
+    public void remove(int key) {
+        int hashKey = key % this.totalKeySpace;
+        this.hashTable.get(hashKey).remove(key);
+    }
+}
+
+class Pair<U, V> {
+    public U first;
+    public V second;
+    
+    public Pair(U first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Bucket {
+    private List<Pair<Integer, Integer>> bucket;
+    
+    public Bucket() {
+        this.bucket = new LinkedList<Pair<Integer, Integer>>();
+    }
+    
+    public Integer get(Integer key) {
+        for (Pair<Integer, Integer> pair : this.bucket) {
+            if (pair.first.equals(key))
+                return pair.second;
+        }
+        return -1;
+    }
+    
+    public void update(Integer key, Integer value) {
+        boolean isFound = false;
+        
+        for (Pair<Integer, Integer> pair : this.bucket) {
+            if (pair.first.equals(key)) {
+                pair.second = value;
+                
+                isFound = true;
+            }
+        }
+        
+        if (!isFound)
+            this.bucket.add(new Pair<Integer, Integer>(key, value));
+    }
+    
+    public void remove(Integer key) {
+        for (Pair<Integer, Integer> pair : this.bucket) {
+            if (pair.first.equals(key)) {
+                this.bucket.remove(pair);
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
