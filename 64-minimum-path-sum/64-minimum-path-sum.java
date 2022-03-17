39902ac48e9205194b3d74471f87b49d8ce1470e
Ahmad Chowdhury <naquib555@gmail.com>
2022-03-16 20:42:56 -0400

    Time: 4 ms (34.71%), Space: 45.3 MB (62.15%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -7,6 +7,24 @@ class Solution {
         m = grid.length;
         n = grid[0].length;
         
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j != n - 1)
+                    grid[i][j] = grid[i][j] + grid[i][j + 1];
+                else if (i != m - 1 && j == n - 1)
+                    grid[i][j] = grid[i][j] + grid[i + 1][j];
+                else if (i != m - 1 && j != n - 1)
+                    grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j], grid[i][j + 1]);
+                else grid[i][j] = grid[i][j];
+            }
+        }
+        return grid[0][0];
+    }
+    
+    public int minPathSum_n2space(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        
         int[][] dp = new int[m][n];
         
         for (int i = m - 1; i >= 0; i--) {
